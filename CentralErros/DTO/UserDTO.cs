7e27e27414484694fceda50253df1a3c92040bd1
M. Bento Do Vale <m.bento.do.vale@avanade.com>
2020-05-01 17:25:32 -0300

    Confirmar senha

--- CentralErros/DTO/UserDTO.cs
+++ CentralErros/DTO/UserDTO.cs
@@ -6,7 +6,8 @@ namespace CentralErros.DTO
 {
     public class UserDTO
     {
-        [Required]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(80, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
@@ -16,6 +17,10 @@ namespace CentralErros.DTO
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "As senhas não conferem.")]
+        public string ConfirmPassword { get; set; }
+
 
 
     }
